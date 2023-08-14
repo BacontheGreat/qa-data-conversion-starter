e390fb9819d6aed1c91ff9f77e619c39461a9c47
BacontheGreat <bacontomas@gmail.com>
2023-08-14 14:24:35 -0500

    added code for main

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,12 +1,48 @@
 public class Data {
-//    Instance variables amount, unit, and downloadSpeed
+    // Instance variables amount, unit, and downloadSpeed
+    private double amount;
+    private String unit;
+    private double downloadSpeed;
 
+    // Constructor
+    public Data(double amount, String unit, double downloadSpeed) {
+        this.amount = amount;
+        this.unit = unit;
+        this.downloadSpeed = downloadSpeed;
+    }
 
-//    Constructor
+    // Class methods
+    public double convertToMegabytes() {
+        if (unit.equals("bytes")) {
+            return amount / 1048576.0;
+        } else if (unit.equals("kilobytes")) {
+            return amount / 1024.0;
+        } else if (unit.equals("megabytes")) {
+            return amount;
+        } else if (unit.equals("gigabytes")) {
+            return amount * 1024.0;
+        } else {
+            throw new IllegalArgumentException("Invalid unit");
+        }
+    }
 
+    public double calculateDownloadTime() {
+        double megabytes = convertToMegabytes();
+        double megabits = megabytes * 8.0;
+        return megabits / downloadSpeed;
+    }
 
-//    Class methods
+    public String getFormattedDownloadTime() {
+        double time = calculateDownloadTime();
+        int minutes = (int) time;
+        int seconds = (int) ((time - minutes) * 60);
+        return minutes + " minutes " + seconds + " seconds";
+    }
 
-
-//    toString using Class methods
+    // toString using Class methods
+    @Override
+    public String toString() {
+        return "Data: " + amount + " " + unit + "\nDownload Time: " + getFormattedDownloadTime();
+    }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Build a scanner
+        Scanner scanner = new Scanner(System.in);
 
         //Prompt and store double amount
+        System.out.print("Enter the amount of data: ");
+        double amount = scanner.nextDouble();
 
         //Prompt and store String unit
+        System.out.print("Enter the unit of data (bytes, kilobytes, megabytes, gigabytes): ");
+        String unit = scanner.next();
 
-        //Prompt and store download speed (in megabits
+        //Prompt and store download speed (in megabits)
+        System.out.print("Enter the download speed (in megabits): ");
+        double speed = scanner.nextDouble();
 
         //Build a Data object with these three values
+        Data data = new Data(amount, unit, speed);
 
         //Print out the data Object with your toString();
+        System.out.println(data.toString());
     }
 }
+
